2496fe7c6eb06806bd3fc59cc997aedb59fc6ac9
Bryan Mauriz Arizaga <bryanmauriz@gmail.com>
2020-02-20 01:10:29 -0500

    First Commit

---
+++ spring-boot-backend-apirest/src/main/java/com/mauriz/springboot/backend/apirest/models/services/ClienteServiceImpl.java
@@ -0,0 +1,24 @@
+package com.mauriz.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mauriz.springboot.backend.apirest.models.entity.Cliente;
+import com.mauriz.springboot.backend.apirest.models.dao.IClienteDao;
+
+@Service
+public class ClienteServiceImpl implements IClienteService {
+
+	@Autowired
+	private IClienteDao clienteDao;
+	
+	@Override
+	@Transactional(readOnly=true)
+	public List<Cliente> findAll() {
+		return (List<Cliente>) clienteDao.findAll();
+	}
+
+}
