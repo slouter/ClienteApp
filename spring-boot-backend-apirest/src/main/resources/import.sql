1fb71ef9d03a023a7d4105f140565d88f03d62e3
bryan <bryanmauriz@gmail.com>
2020-03-10 09:57:20 -0500

    SegundoCommit

---
+++ spring-boot-backend-apirest/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into clientes (nombre,apellido,email,create_at) values ('bryan christhope','mauriz arizaga','bryanmauriz@gmail.com','2020-03-09');
+insert into clientes (nombre,apellido,email,create_at) values ('stephnaie grace','mauriz arizaga','smauriz0206@gmail.com','2020-03-09');
+insert into clientes (nombre,apellido,email,create_at) values ('elena milagros','arizaga gavino','elena-sbc@hotmail.com','2020-02-15');
+insert into clientes (nombre,apellido,email,create_at) values ('cristobal alfredo','mauriz changra','cmaurizch2011@hotmail.com','2020-02-15');
+insert into clientes (nombre,apellido,email,create_at) values ('gianny milagros','huamani flores','gianny190@gmail.com','2020-03-09');
